b5cce690430775263207660ed291abd7d0ff220d
PHxinhdep <phanthiphuochanh04@gmai.com>
2025-04-22 14:34:35 +0700

    feat(device-service):map api

--- admin/assets/js/device.js
+++ admin/assets/js/device.js
@@ -1,6 +1,7 @@
-var DeviceAPI = 'http://localhost:3000/device';
-var DeviceTypeAPI = 'http://localhost:3000/device_type';
-var UsersAPI = 'http://localhost:3000/user';
+const API_BASE = 'http://localhost:8080/event-management';
+const DeviceAPI = `${API_BASE}/devices`;
+const DeviceTypeAPI = `${API_BASE}/deviceType`;
+const UsersAPI = `${API_BASE}/users`;
 function start() {
     getData((devices, deviceTypes, users) => {
         renderDevices(devices, deviceTypes, users)
@@ -21,19 +22,9 @@ function getData(callback) {
     }
 
     Promise.all([
-        fetch(DeviceAPI, {
-            headers: {
-                "Authorization": `Bearer ${token}`,
-                "Content-Type": "application/json"
-            }
-        }).then(res => res.json()),
+        fetch(`${DeviceAPI}/list`).then(res => res.json()),
 
-        fetch(DeviceTypeAPI, {
-            headers: {
-                "Authorization": `Bearer ${token}`,
-                "Content-Type": "application/json"
-            }
-        }).then(res => res.json()),
+        fetch(`${DeviceTypeAPI}/list`).then(res => res.json()),
 
         fetch(UsersAPI, {
             headers: {
@@ -81,8 +72,8 @@ function renderDevices(devices, deviceTypes, users) {
                 <td>${deviceTypeName}</td>
                 <td>${device.description || "Không có mô tả"}</td>
                 <td>${device.quantity || 0}</td>
-                <td>${device.hourly_rental_fee ? device.hourly_rental_fee.toLocaleString() + " VND" : "Không xác định"}</td>
-                <td>${device.created_at || "Không xác định"}</td>
+                <td>${device.hourlyRentalFee ? device.hourlyRentalFee.toLocaleString() + " VND" : "Không xác định"}</td>
+                <td>${device.created_at ? new Date(device.created_at).toLocaleDateString("en-US", {year: "2-digit", month: "2-digit", day: "2-digit"}) : "Không xác định"}</td>
                 <td>${device.place || "ko có địa điểm"}</td>
                 <td>${supplierName}</td>
                 <td class="text-center">
